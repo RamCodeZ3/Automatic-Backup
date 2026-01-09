42fd7482c0d62de10a06dcd914879ab1314f8a55
RamCodeZ3 <arammusset7@gmail.com>
2026-01-09 12:06:50 -0400

    feat: Se creo la ui base de la app con customTkinter

--- src/main.py
+++ src/main.py
@@ -1,14 +1,8 @@
 from utils.backup import Backup
 from service.config_service import ConfigService
+from ui.app_ui import App
 
 
 if __name__ == "__main__":
-    
-    b = Backup()
-    c = ConfigService()
-    
-    if c.get_key_value('backupType') == 'local':
-        b.local_backup('C:\\Users\\HP ELITEBOOK 840 G5\\Pictures\\PNG', 'C:\\Users\\HP ELITEBOOK 840 G5\\Desktop\\backup')
-    
-    elif c.get_key_value('backupType') == 'drive':
-        b.google_drive_backup("C:\\Users\\HP ELITEBOOK 840 G5\\Pictures\\PNG") 
+    app = App()
+    app.mainloop()
